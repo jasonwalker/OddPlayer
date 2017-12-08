73ed2a2f6995de3c047bdc0a7a2b37ea4336c399
Jason M Walker <jasonmoorewalker@gmail.com>
2017-12-07 22:10:55 -0500

    change to new crash reporter

--- app/src/main/java/com/jmw/rd/oddplay/OddPlayerApp.java
+++ app/src/main/java/com/jmw/rd/oddplay/OddPlayerApp.java
@@ -6,7 +6,7 @@ import org.acra.ACRA;
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
-@ReportsCrashes(formKey = "", // will not be used
+@ReportsCrashes(// will not be used
         mailTo = "androidoddplayer@gmail.com",
         mode = ReportingInteractionMode.TOAST,
         resToastText = R.string.crash_toast_text)
